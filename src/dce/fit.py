15ae7fb042cb8c8bdca1c319d929718b04d7896b
mjt320 <36667519+mt@users.noreply.github.com>
2020-12-04 21:36:10 +0000

    dce update

--- src/dce/fit.py
+++ src/dce/fit.py
@@ -34,6 +34,11 @@ r_to_s = {
     'spgr': r_to_s_spgr
 }
 
+def s_to_e(s, base_idx):
+    s_pre = np.mean(s[base_idx])
+    e = 100.*((s - s_pre)/s_pre)
+    return e
+
 def e_to_c(e, r_0, rlxy_pars, acq_pars, c_to_r_model, r_to_s_model):
     res = root(lambda c: e - c_to_e(c, r_0, rlxy_pars, acq_pars, c_to_r_model, r_to_s_model),
                x0= 0., method='hybr', options={'maxfev': 1000, 'xtol': 1e-7})
@@ -56,38 +61,62 @@ def pkp_to_c(t, c_p_aif, pk_pars, irf_model):
     c_t = pk_pars['vp'] * c_cp + pk_pars['ve'] * c_e
     return c_cp, c_e, c_t
 
-def pkp_to_s(t, c_p_aif, pk_pars, s0, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model):
-    c_cp, c_e, c_t = pkp_to_c(t, c_p_aif, pk_pars, irf_model)
-    r = c_to_r[c_to_r_model](c_t, r_0, rlxy_pars)
-    s = r_to_s[r_to_s_model](acq_pars, s0, r)
+def c_to_r(c_b, c_e, r_0_cp, r_0_e, c_to_r_model, rlxy_pars):
+    return r
+
+def r_to_comps(r, p, water_ex_model):
+    return r_comps, p_comps
+
+def comps_to_s(r_comps, p_comps, acq_pars, signal_model):
     return s
 
+def pkp_to_s(t, c_p_aif, pk_pars, s0, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model):
+    c_cp, c_e, c_t = pkp_to_c(t, c_p_aif, pk_pars, irf_model)
+    #r = c_to_r[c_to_r_model](c_t, r_0, rlxy_pars)
+    #s = r_to_s[r_to_s_model](acq_pars, s0, r)
+    r = c_to_r(c_b, c_e, r_0_cp, r_0_e, c_to_r_model, rlxy_pars)
+    r_comps, p_comps = r_to_comps(r, p, water_ex_model)
+    s = comps_to_s(r_comps, p_comps, acq_pars, signal_model)
+    return s
 
-def pkp_to_x(pk_pars, s0, fix_pars, irf_model):
+def pkp_to_x(pk_pars, s0, irf_model):
     # create vector x listing values in pk_pars dict (in standard order), followed by s0
-    x = [pk_pars[par] for par in models.req_pars[irf_model] if par not in fix_pars.keys() ] + [s0]
+    x = np.asarray([pk_pars[par] for par in models.req_pars[irf_model]] + [s0], dtype=np.float32)
     return x
 
-def x_to_pkp(x, fix_pars, irf_model):
+def x_to_pkp(x, irf_model):
     # convert vector x of parameter values to pk_pars dict and s0 value
-    parnames_in_x = [parname for parname in models.req_pars[irf_model] if parname not in fix_pars.keys()] #list pk parameter names corresponding to elements of x
-    pk_pars = { **{parname:x[n] for n, parname in enumerate(parnames_in_x)}, # dictionary containg pk parameters from x
-               **{parname:value for parname, value in fix_pars.items()} } # concatenate dictionary containing fixed parameters
+    parnames_in_x = [parname for parname in models.req_pars[irf_model]] #list pk parameter names corresponding to elements of x
+    pk_pars = {parname:x[n] for n, parname in enumerate(parnames_in_x)} # dictionary containing pk parameters from x
     s0 = x[-1]
     return pk_pars, s0
 
-def obj_fun(x, fix_pars, t, s, c_p_aif, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model):
-    pk_pars_try, s0_try = x_to_pkp(x, fix_pars, irf_model)
+def obj_fun(x, x_sf, t_mask, t, s, c_p_aif, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model):
+    pk_pars_try, s0_try = x_to_pkp(x * x_sf, irf_model)
     s_try = pkp_to_s(t, c_p_aif, pk_pars_try, s0_try, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model)
-    ssq = np.sum((s_try - s)**2) #add masking later
+    ssq = np.sum(t_mask*((s_try - s)**2)) #add masking later
     return ssq
 
-# NEXT STEP: ENCODE BOUNDS AND CONSTRAINTS, IMPLEMENT MULTI START, CHECK NEED FOR SCALING VARIABLES
+# TO DO:
+# INTERPOLATE BEFORE CONVOLUTION
+# CHECK SPEED IF PARAMETERS FIXED VIA BOUNDS
+# ADD WATER EXCHANGE
+# ADD LINEAR CONSTRAINTS
+# ADD MULTISTART
 def s_to_pkp(t, s, c_p_aif, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model, fit_opts):
-    x0 = pkp_to_x(fit_opts['pk_pars_0'], fit_opts['s0_0'], fit_opts['fix_pars'], irf_model)
-    res = minimize(obj_fun, x0, args=(fit_opts['fix_pars'], t, s, c_p_aif, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model),
-             method='trust-constr', bounds=[(0., 1.), (0., None), (0., None)])
-    pk_pars_opt = {'vp': res.x[0], 'ps': res.x[1], 've': 1.}
-    return pk_pars_opt
+    s0_0 = s[0] / r_to_s[r_to_s_model](acq_pars, 1., r_0)
+    print(s0_0)
+    x_0 = pkp_to_x(fit_opts['pk_pars_0'], s0_0, irf_model) 
+    x_sf= x_0 # use initial values to scale variables    
+    x_lb_norm = pkp_to_x(fit_opts['pk_pars_lb'], s0_0*0.5, irf_model) / x_sf
+    x_ub_norm = pkp_to_x(fit_opts['pk_pars_ub'], s0_0*1.5, irf_model) / x_sf
+    x_0_norm = x_0/x_sf
+    
+    bounds = list(zip(x_lb_norm, x_ub_norm))    
+    res = minimize(obj_fun, x_0_norm, args=(x_sf, fit_opts['t_mask'], t, s, c_p_aif, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model),
+             method='trust-constr', bounds=bounds)#method='trust-constr', bounds=bounds, constraints=models.pkp_constraints[irf_model])
+    pk_pars_opt, s0_opt = x_to_pkp(res.x * x_sf, irf_model)
+    s_fit = pkp_to_s(t, c_p_aif, pk_pars_opt, s0_opt, r_0, rlxy_pars, acq_pars, irf_model, c_to_r_model, r_to_s_model)
+    return pk_pars_opt, s0_opt, s_fit
 
 
--- src/dce/models.py
+++ src/dce/models.py
@@ -6,15 +6,19 @@ Created on Sat Nov  7 21:05:18 2020
 """
 
 import numpy as np
+from scipy.optimize import LinearConstraint
 
-req_pars = {'patlak': ('vp', 'ps', 've'),
+req_pars = {'patlak': ('vp', 'ps', 've'), # (v_e required to determine local EES concentration)
             }
 
+pkp_constraints = {'patlak': LinearConstraint([[1., 0., 0., 0.],[1., 0., 1., 0.]], [0., 0.], [1., 1.])}
+
 def irf_patlak(t, pk_pars):
+    dt = t[1]-t[0] #to do: generalise this
     n = t.size
     h_cp = np.zeros(n, dtype=float)
-    h_cp[0] = 1. * pk_pars['vp']    
-    h_e = np.ones(n, dtype=float) * ( pk_pars['ps'] / pk_pars['ve'] )
+    h_cp[0] = 1./dt
+    h_e = np.ones(n, dtype=float) * (1./60.) * ( pk_pars['ps'] / pk_pars['ve'] )
     h_e[0] = h_e[0]/2.
     return h_cp, h_e
 
