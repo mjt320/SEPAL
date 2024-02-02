74d143d40a049d3907150a166b996aae8d3dd306
mjt320 <mjt320@googlemail.com>
2024-02-02 18:04:10 +0000

    implemented inversion recovery T1 fitting

--- src/signal_models.py
+++ src/signal_models.py
@@ -7,6 +7,7 @@ Created 28 September 2020
 
 Classes: SignalModel and derived subclasses:
     SPGR
+    IRSE
 """
 
 from abc import ABC, abstractmethod
@@ -74,3 +75,38 @@ class SPGR(SignalModel):
                   (1.0-np.exp(-self.tr*R1)*np.cos(fa))
                   ) * np.exp(-self.te*R2s)
         return s
+
+
+class IRSE(SignalModel):
+    """Signal model subclass for inversion-recovery spin-echo pulse sequence.
+    Ignores impact of refocusing pulse on Mz.
+
+    """
+
+    def __init__(self, tr, ti, fa_ex, fa_inv, te, signed_signal=False):
+        """
+
+        Args:
+            tr (float): repetition time (s)
+            ti (float): inversion time (s)
+            fa_ex (float): excitation flip angle (deg)
+            fa_inv (float): inversion flip angle (deg)
+            te (float): echo time (s)
+            signed_signal (bool): whether signal can be + or -
+        """
+        self.tr = tr
+        self.ti = ti
+        self.fa_ex = fa_ex * np.pi/180
+        self.fa_inv = fa_inv * np.pi/180
+        self.te = te
+        self.signed_signal = signed_signal
+
+    def R_to_s(self, s0, R1, R2=0, R2s=None, k_fa=1):
+        """Get signal for this model. Overrides superclass method."""
+        fa_ex = k_fa * self.fa_ex
+        fa_inv = k_fa * self.fa_inv
+        s = s0 * np.exp(-self.te*R2) * np.sin(fa_ex) * (
+                1-np.cos(fa_inv)*np.exp(-self.tr*R1)-(1-np.cos(fa_inv))
+                * np.exp(-self.ti*R1)) / (
+                1-np.cos(fa_inv)*np.cos(fa_ex)*np.exp(-self.tr*R1))
+        return s if self.signed_signal else np.abs(s)
